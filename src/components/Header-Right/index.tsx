2c7107dc7cbfa53190d53a40bc08f7c7d77d0aeb
Thiago Porfirio <thiagovporfirio@gmail.com>
2023-01-07 17:31:25 -0300

    adicionando usestate aos components

--- src/components/Header-Right/index.tsx
+++ src/components/Header-Right/index.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react'
+
 import { Container } from "./styles"
 import { UserColaborador } from "../UserColaborador"
 import { Form } from "../FormLogin"
@@ -9,10 +11,15 @@ export function Right() {
     let identidadeUser = JSON.parse(localStorage.getItem('_DadosUser') || '{}')
     let usuarioid = identidadeUser?.dados?.UserId
 
+
+    const handleProducts = () => {
+
+    }
+
     return (
         <Container >
             <div className="content-btn">
-                <button id="btnlogin">Produtos</button>
+                <button id="btnlogin" onClick={handleProducts}>Produtos</button>
                 <button id="btncadastraUser" >Cadastrar user</button>
             </div>
 
